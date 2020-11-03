cba010f4d54ed63a02b918da5d18021cd57ebbb9
Aliesso <akogamer17@gmail.com>
2020-11-03 23:08:33 +0400

    Coretto

--- js/app.js
+++ js/app.js
@@ -1,21 +1,45 @@
 'use stri t';
 var sliderWidth = document.querySelector(".slider").clientWidth;
 var sliderLength = document.querySelectorAll(".slider li ").length
-var mySlideWidth = sliderWidth * sliderLength;
 var sliderUl = document.querySelector(".slider ul")
 var nextIco = document.querySelector(".next")
 var prevIco = document.querySelector(".prev")
-var c = 0;
+var cloneFirstSlider = document.querySelector(".slider li").cloneNode(true)
+var cloneLastSlider = document.querySelectorAll(".slider li")[sliderLength - 1].cloneNode(true)
+document.querySelector(".slider ul").appendChild(cloneFirstSlider)
+document.querySelector(".slider ul").prepend(cloneLastSlider)
+sliderLength = document.querySelectorAll(".slider li").length
+var mySlideWidth = sliderWidth * sliderLength;
+
+var c = 1;
+
+sliderUl.style.left = -(c * sliderWidth) + "px"
+
 sliderUl.style.width = mySlideWidth + "px"
 prevIco.onclick = function() {
-    clearInterval(myInt)
+    prevIco.classList.add("disabled")
+    sliderUl.style.transition = ".s ease"
     myPrevSlider();
-    setInterval(myNextSlider, 5000);
+
 }
+document.addEventListener("transitionend", function() {
+    nextIco.classList.remove("disabled")
+    prevIco.classList.remove("disabled")
+    if (c == sliderLength - 1) {
+        sliderUl.style.transition = "none";
+        c = 1
+        sliderUl.style.left = -(c * sliderWidth) + "px"
+    } else if (c == 0) {
+        sliderUl.style.transition = "none";
+        c = sliderLength - 2
+        sliderUl.style.left = -(c * sliderWidth) + "px"
+    }
+})
 nextIco.onclick = function() {
-    clearInterval(myInt)
+    nextIco.classList.add("disabled")
+    sliderUl.style.transition = ".7s ease"
     myNextSlider();
-    setInterval(myNextSlider, 5000);
+
 }
 
 function myPrevSlider() {
@@ -35,4 +59,12 @@ function myNextSlider() {
     sliderUl.style.left = -(c * sliderWidth) + "px"
 
 }
-var myInt = setInterval(myNextSlider, 6000);
+var myInt = setInterval(myNextSlider, 4000);
+
+window.onscroll = function() {
+    if (document.documentElement.scrollTop > 750) {
+        header.classList.add("head-active")
+    } else {
+        header.classList.remove("head-active")
+    }
+}
